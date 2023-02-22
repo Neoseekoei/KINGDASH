0da638083acc8e38af4077930d9de308be3129d5
Neoseekoei <seekoeineo16@gmail.com>
2023-02-22 10:49:17 -0800

    third commit

---
+++ myscritpt.js
@@ -0,0 +1,48 @@
+window.onload = function (){
+
+ var surname = "Seekoei"
+ var name = "Neo"
+ var bio = "My goal is to learn everything about WEB DEV and master it so i can grow "<br>"the future of the world is online and we will soon be doing everthing on uor computers and phones."
+
+ var project = ["HTML" ,"CSS" ,"JAVASCRIPT"]
+
+ var phone = "0791999079"
+
+ document.getElementById("name").innerHTML=name;
+
+ document.getElementById("surname").innerHTML=surname; 
+ 
+ document.getElementById("phonenumber").innerHTML=phonenumber;
+
+let skillslist = [
+  {img:'./background/html-5.png' ,item: "HTML"},
+  {img:'background/css-3.png' ,item: "CSS"},
+  {img:'background/js.png' ,item: "JAVAScript"}
+]
+
+let listUl = "<div>";
+for (let i =0; i <skillslist.length; i++) [
+  console.log(skillsList[i].item);
+  listUl +=
+  "<table border='l'><tr><td><img src='"+skillslist[i].img+'" alt= >' skillsList[i].item + "
+]
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
